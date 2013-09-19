9aa756f5d96ce58e0ba67bf1aa4eda8a9a7ae6bb
Jonathan Eccles <dev@ecclesagile.com>
2013-09-19 08:30:56 -0400

    Initial revision

---
+++ src/App.js
@@ -0,0 +1,26 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+  items:{
+        html:'<a href="https://help.rallydev.com/apps/2.0rc1/doc/#!/api/Rally.ui.cardboard.CardBoard">Hint!<a>'
+    },
+
+    launch: function() {
+        var cardBoardConfig = {
+            xtype: 'rallycardboard',
+            types: ['User'],
+            attribute: "Department", 
+            cardConfig:{
+                xtype: 'customcard'
+            },
+            enableCrossColumnRanking: false,
+            enableRanking: false,
+            storeConfig: {
+                property: 'ObjectID',
+                direction: 'ASC'
+            }
+         };
+
+        this.add(cardBoardConfig);
+    }
+});
---
+++ src/CustomCard.js
@@ -0,0 +1,30 @@
+
+Ext.define('App.MyCard', {
+    //This is the class we are extending. In this case we are making a custom card extending Rally's default card
+    extend: 'Rally.ui.cardboard.Card',
+    //the alias is used to create a reference for component's xtypes
+    alias: 'widget.customcard',
+
+    //This record will have the data for the Rally Artifact when the card is created.
+    record:null,
+    inheritableStatics: {
+        //This list of fields you expect hydrated in the records. This information is passed along to the Rally server and assures that
+        getFetchFields: function() {
+            return ['UserName'];
+        }
+    },
+    
+    initComponent:function(){
+        this.addField("UserName");
+        // this.addField("Parent");
+        // if(this.record.get("Parent")) {
+        //     console.log(this.record.get("FormattedID"));
+        //     console.log(this.record.get("Parent"));
+        //     this.addCls("parented");
+        // }
+        this.callParent(arguments);
+        console.log(this.record.data);
+    }
+    
+    
+});
